22dce339246a8dabd4c8bad890547080d23ef32b
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2022-12-20 10:07:45 -0500

    first files

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "oguzhanaydogan"
+
+    workspaces {
+      name = "terraformcloud"
+    }
+  }
+}
